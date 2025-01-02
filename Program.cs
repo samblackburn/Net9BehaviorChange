e6f265e30327151101a06b95bff7dd21416dfd30
Sam Blackburn <sam.blackburn@red-gate.com>
2025-01-02 11:27:59 +0000

    Simplify

--- Program.cs
+++ Program.cs
@@ -6,63 +6,20 @@ public abstract class Program
     public static void Main()
     {
         var dateTimeOffsetData = new byte[] { 84, 80, 1, 37, 53, 11, 10, 0};
-        GetBinaryDateTimeOffsetString(
-            dateTimeOffsetData,
-            0,
-            8,
-            0);
+        GetBinaryDateTimeOffsetString(dateTimeOffsetData, 0, 0);
     }
 
     private static void GetBinaryDateTimeOffsetString(
         byte[] data,
         byte prec,
-        byte len,
         byte scale)
     {
-        SS2008DateTime2(data, (byte)(prec - 7), (byte)(len - 2), scale);
-    }
-
-    private static void SS2008DateTime2(
-        byte[] data,
-        byte precision,
-        byte length,
-        byte scale)
-    {
-        var m_Time = SS2008Time(data, precision - 11, length - 3, scale);
-        Console.WriteLine($"m_Time: {m_Time.m_Hour}:{m_Time.m_Minute}:{m_Time.m_Second}.{m_Time.m_SubSecond}");
-    }
-
-    private static SqlServer2008Time SS2008Time(
-        byte[] data,
-        int precision,
-        int length,
-        int scale)
-    {
+        var precision = (byte)(prec - 7);
         var time = new SqlServer2008Time();
         double subsecond;
-        ulong value;
-        var divider = (ulong)Math.Pow(10, precision - 9);
-
-        switch (length)
-        {
-            case 3:
-            {
-                value = (ulong)(BitConverter.ToUInt16(data, 0) | (data[2] << 16));
-            }
-                break;
-            case 4:
-            {
-                value = BitConverter.ToUInt32(data, 0);
-            }
-                break;
-            case 5:
-            {
-                value = BitConverter.ToUInt32(data, 0) | ((ulong)data[4] << 32);
-            }
-                break;
-            default:
-                throw new Exception();
-        }
+        var divider = (ulong)Math.Pow(10, precision - 11 - 9);
+        var value = (ulong)(BitConverter.ToUInt16(data, 0) | (data[2] << 16));
+        //Console.WriteLine($"value: {value}");
 
         if (divider == 0)
         {
@@ -86,7 +43,7 @@ public abstract class Program
         }
 
         time.m_SubSecond = (int)Math.Round(subsecond);
-        return time;
+        Console.WriteLine($"time: {time.m_Hour}:{time.m_Minute}:{time.m_Second}.{time.m_SubSecond}");
     }
 
     private class SqlServer2008Time
